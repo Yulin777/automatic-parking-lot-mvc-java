86365f3cc56995c643e2635b2ae0a0874d4935ee
Genia-m <37803232+Genia-m@users.noreply.github.com>
2018-06-05 16:31:14 +0300

    rutine workercontroll backup

--- CPS/src/CPS/WorkerController.java
+++ CPS/src/CPS/WorkerController.java
@@ -1,5 +1,8 @@
 package CPS;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class WorkerController {
 	
 	private String worker_Fname;
@@ -18,4 +21,18 @@ public class WorkerController {
 		return null;
 		 
 	 }
+	 
+	  static String sha1(String input) throws NoSuchAlgorithmException {
+	        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+	        byte[] result = mDigest.digest(input.getBytes());
+	        StringBuffer sb = new StringBuffer();
+	        for (int i = 0; i < result.length; i++) {
+	            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	         
+	        return sb.toString();
+	    }
+	 
+	 
+	 
 }
