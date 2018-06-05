041cdd154131c9d076bb3988b468a5f7c2dc5018
Genia-m <37803232+Genia-m@users.noreply.github.com>
2018-06-05 16:27:47 +0300

    worker controller

--- CPS/src/CPS/WorkerController.java
+++ CPS/src/CPS/WorkerController.java
@@ -2,8 +2,19 @@ package CPS;
 
 public class WorkerController {
 	
+	private String worker_Fname;
+	private String worker_Lname;
+	private String worker_email;
+	private String worker_password;
+	private String worker_phone;
+	private String worker_access_level;
+
+	
 	//TODO Evgeny
-	 public static Worker getWorker(String email, String password){
+	 public static Worker getWorker(String email, String password)
+	 {
+		 
+		 
 		return null;
 		 
 	 }
