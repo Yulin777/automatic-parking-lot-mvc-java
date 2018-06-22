6e7e03732c32e14bae8cdeb5f285658ad5292a2e
tyizchak <taliayit@gmail.com>
2018-06-22 23:14:24 +0300

    fixed prev push

--- CPS_server/src/server/Server.java
+++ CPS_server/src/server/Server.java
@@ -21,7 +21,6 @@ public class Server {
 				String s = br.readLine();
 				System.out.println("[request] " + s);
 				handelRequest(s, currentSocket);
-				OrderController.calcPrice("21");
 			}
 		} catch (Exception ioe){
 			System.out.println(ioe.getMessage());
