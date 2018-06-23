ed6b86e3fbad984e39e32c65ce30ee86eefd4c17
Genia-m <37803232+Genia-m@users.noreply.github.com>
2018-06-23 15:36:44 +0300

    loginview update

--- CPS_client/src/CPS/View/mainView.java
+++ CPS_client/src/CPS/View/mainView.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import client.Client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -47,6 +48,20 @@ public class mainView extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        Client myClient = new Client();
+        myClient.addNewComplaint("1", "some long long long long"
+        		+ "long long long longlong long long long long long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long"
+        		+ "long long long longlong long long longlong long long longlong long long long text"
+        		
+        		);
+        System.out.println("helllooooooooooooooo");
     }
 
 
