e47965c4e212464cccb4d43b0b39a11f78fb27ee
Ivan Yulin <yulin.i777@gmail.com>
2018-06-24 04:37:40 +0300

    javadoc

--- CPS_server/src/server/Location.java
+++ CPS_server/src/server/Location.java
@@ -4,31 +4,18 @@ public class Location {
 	int level;
 	int row;
 	int column;
-	
+
+	/**
+	 * constructor
+	 *
+	 * @param level  in parking lot
+	 * @param row    in parking lot
+	 * @param column in parking lot
+	 */
 	public Location(int level, int row, int column) {
 		this.level = level;
 		this.row = row;
 		this.column = column;
 	}
-	
-	public int getLevel() {
-		return level;
-	}
-	public void setLevel(int level) {
-		this.level = level;
-	}
-	public int getRow() {
-		return row;
-	}
-	public void setRow(int row) {
-		this.row = row;
-	}
-	public int getColumn() {
-		return column;
-	}
-	public void setColumn(int column) {
-		this.column = column;
-	}
-	
-	
+
 }
