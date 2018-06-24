392b0b75e2050b433faa76bdb6240ce4ae38d8e8
Ivan Yulin <yulin.i777@gmail.com>
2018-06-24 05:16:20 +0300

    java doc

--- CPS_server/src/server/OrderController.java
+++ CPS_server/src/server/OrderController.java
@@ -6,7 +6,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class OrderController {
-	ParkingStationController psc =new ParkingStationController();
+	ParkingStationController psc = new ParkingStationController();
+
 	public enum OrderStatus {
 		PENDING, ONGOING;
 	}
@@ -23,6 +24,12 @@ public class OrderController {
 	private static server.sqlConnection sql = server.sqlConnection.getInstant();
 
 
+	/**
+	 * finds the status of the order by give order id
+	 *
+	 * @param order_id
+	 * @return the status of current order
+	 */
 	public static String getOrderStatus(int order_id) {
 		java.sql.PreparedStatement stmt = null;
 
@@ -37,12 +44,20 @@ public class OrderController {
 			}
 			return rs.getString("order_status");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+	/**
+	 * adds new subscription to db
+	 *
+	 * @param cliendID
+	 * @param carID
+	 * @param startDate
+	 * @param endDate
+	 * @return result string
+	 */
 	public static String addNewSubscription(String cliendID, String carID, java.sql.Timestamp startDate,
 											java.sql.Timestamp endDate) {
 		Statement stmt;
@@ -67,20 +82,8 @@ public class OrderController {
 
 				System.out.println("New subscription was added succsfully");
 
-				if (uprs != null) {
-					try {
-						uprs.close();
-					} catch (SQLException e) {
-						/* ignored */
-					}
-				}
-				if (stmt != null) {
-					try {
-						stmt.close();
-					} catch (SQLException e) {
-						/* ignored */
-					}
-				}
+				uprs.close();
+				stmt.close();
 			} else {
 				return ("client already has subscription");
 			}
@@ -91,40 +94,50 @@ public class OrderController {
 		return ("New subscription was added succsfully");
 	}
 
+	/**
+	 * adds an order for occasional clients
+	 * the method adds a client, a car and an order if any does not exist already
+	 *
+	 * @param carID
+	 * @param endDate
+	 * @param parkingName
+	 * @param paymentMethod
+	 * @return true on successful add. false otherwise
+	 */
 	public static boolean addOccasionalOrder(String carID, String endDate, String parkingName, String paymentMethod) {
 		boolean flag = false;
 		PreparedStatement stmt;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-	
+
 		try {
 			stmt = sql.conn.prepareStatement("SELECT * FROM orders WHERE order_car_id=?");
 			stmt.setString(1, carID);
 			ResultSet client = stmt.executeQuery();
 			if (!client.next() || (client.next() && !OrderOverlaps(carID, client.getString(7), endDate, parkingName))) {
-				if(ParkingStationController.insertCar(getOrderParkingId(parkingName),LocalDateTime.parse(endDate, formatter), Integer.parseInt(carID))==true){
+				if (ParkingStationController.insertCar(getOrderParkingId(parkingName), LocalDateTime.parse(endDate, formatter), Integer.parseInt(carID)) == true) {
 
-				stmt = sql.conn.prepareStatement("INSERT INTO orders (order_status,order_car_id,order_type,end_date,order_parking_id,order_payment_method) VALUES (?,?,?,?,?,?)"
-						, Statement.RETURN_GENERATED_KEYS);
+					stmt = sql.conn.prepareStatement("INSERT INTO orders (order_status,order_car_id,order_type,end_date,order_parking_id,order_payment_method) VALUES (?,?,?,?,?,?)"
+							, Statement.RETURN_GENERATED_KEYS);
 
-				stmt.setString(1, OrderStatus.PENDING.toString());
-				stmt.setString(2, carID);
-				stmt.setString(3, OrderType.OCCASIONAL.toString());
-				stmt.setString(4, endDate);
-				stmt.setInt(5, getOrderParkingId(parkingName));
-				stmt.setString(6, paymentMethod);
-				stmt.executeUpdate();
+					stmt.setString(1, OrderStatus.PENDING.toString());
+					stmt.setString(2, carID);
+					stmt.setString(3, OrderType.OCCASIONAL.toString());
+					stmt.setString(4, endDate);
+					stmt.setInt(5, getOrderParkingId(parkingName));
+					stmt.setString(6, paymentMethod);
+					stmt.executeUpdate();
 
-				ResultSet uprs = stmt.getGeneratedKeys();
-				if (uprs.next()) {
-					calcAndUpdatePrice(uprs.getInt(1));
-				}
+					ResultSet uprs = stmt.getGeneratedKeys();
+					if (uprs.next()) {
+						calcAndUpdatePrice(uprs.getInt(1));
+					}
 
-				System.out.println("New order was added successfully");
-				flag = true;
+					System.out.println("New order was added successfully");
+					flag = true;
 
-				uprs.close();
-				stmt.close();
-				}else {
+					uprs.close();
+					stmt.close();
+				} else {
 					System.out.println("order already exists for current car");
 				}
 			}
@@ -134,6 +147,14 @@ public class OrderController {
 		return flag;
 	}
 
+	/**
+	 * finds parking id by its name
+	 *
+	 * @param parkingName
+	 * @return parking id or -1 if was not found.
+	 * it may be not possible to return the value -1 since the name is selected from existing names in db
+	 * but the value is initiated for testing purposes
+	 */
 	public static int getOrderParkingId(String parkingName) {
 		int res = -1;
 		PreparedStatement stmt;
@@ -152,6 +173,17 @@ public class OrderController {
 		return res;
 	}
 
+	/**
+	 * adds an order for clients that wish to reserve a parking slot
+	 * the method adds a client, a car and an order if any does not exist already
+	 *
+	 * @param carID
+	 * @param startDate
+	 * @param endDate
+	 * @param parkingName
+	 * @param paymentMethod
+	 * @return true if add was successful. false otherwise
+	 */
 	public static boolean addInAdvanceOrder(String carID, String startDate, String endDate, String parkingName, String paymentMethod) {
 		boolean flag = false;
 		PreparedStatement stmt;
@@ -159,7 +191,7 @@ public class OrderController {
 			stmt = sql.conn.prepareStatement("SELECT * FROM orders WHERE order_car_id=?");
 			stmt.setString(1, carID);
 			ResultSet client = stmt.executeQuery();
-			if ((OrderOverlaps(carID, startDate, endDate, parkingName) == false)&&(ParkingStationController.checkAvilablePlace(getOrderParkingId(parkingName), startDate, endDate)==true)) {
+			if ((!OrderOverlaps(carID, startDate, endDate, parkingName)) && (ParkingStationController.checkAvilablePlace(getOrderParkingId(parkingName), startDate, endDate))) {
 
 				stmt = sql.conn.prepareStatement("INSERT INTO orders (order_status,order_car_id,order_type,start_date,end_date,order_parking_id,order_payment_method ) VALUES (?,?,?,?,?,?,?)"
 						, Statement.RETURN_GENERATED_KEYS);
@@ -221,31 +253,29 @@ public class OrderController {
 			while (rs.next()) {
 //				int numColumns = rs.getMetaData().getColumnCount();
 //				for (int i = 1; i <= numColumns; i++) {
-					// Column numbers start at 1.
-					// Also there are many methods on the result set to return
-					//  the column as a particular type. Refer to the Sun documentation
-					//  for the list of valid conversions.
+				// Column numbers start at 1.
+				// Also there are many methods on the result set to return
+				//  the column as a particular type. Refer to the Sun documentation
+				//  for the list of valid conversions.
 
-					Timestamp order_start_time = rs.getTimestamp(7);
-					Timestamp order_end_time = rs.getTimestamp(8);
-					if (parkingName.equals(ParkingStationController.getParkingNameByID(rs.getInt(6)))) {
+				Timestamp order_start_time = rs.getTimestamp(7);
+				Timestamp order_end_time = rs.getTimestamp(8);
+				if (parkingName.equals(ParkingStationController.getParkingNameByID(rs.getInt(6)))) {
 
-						if (start_date_timestamp.after(order_start_time) && start_date_timestamp.before(order_end_time)) {
-							System.err.println("order failed. start time overlaps existing order");
-							return true;
-						}
-						if (end_date_timestamp.after(order_start_time) && end_date_timestamp.before(order_end_time)) {
-							System.err.println("order failed. end time overlaps existing order");
-							return true;
-						}
+					if (start_date_timestamp.after(order_start_time) && start_date_timestamp.before(order_end_time)) {
+						System.err.println("order failed. start time overlaps existing order");
+						return true;
 					}
-//				}
+					if (end_date_timestamp.after(order_start_time) && end_date_timestamp.before(order_end_time)) {
+						System.err.println("order failed. end time overlaps existing order");
+						return true;
+					}
+				}
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		return false;
 	}
 
@@ -279,6 +309,13 @@ public class OrderController {
 		return false;
 	}
 
+	/**
+	 * method allows client to park his car.
+	 * changes order status to ongoing on success
+	 *
+	 * @param carID
+	 * @return true if order is in status pending and the current time matches order start time. false otherwise
+	 */
 	public static boolean startParking(String carID) {
 		boolean flag = false;
 		int res = 0;
@@ -293,8 +330,8 @@ public class OrderController {
 				Timestamp startTime = rs.getTimestamp(7);
 				Timestamp endTime = rs.getTimestamp(8);
 				Timestamp nowTime = new Timestamp(System.currentTimeMillis());
-				if(getTimeDiffInHours(startTime, nowTime) >= 0 && getTimeDiffInHours(nowTime, endTime) > 0) {
-	
+				if (getTimeDiffInHours(startTime, nowTime) >= 0 && getTimeDiffInHours(nowTime, endTime) > 0) {
+
 					stmt = sql.conn.prepareStatement("UPDATE  `Group_1`.`orders` SET  `order_status` =  ? WHERE  `orders`.`order_id` =?;");
 					stmt.setString(1, OrderStatus.ONGOING.toString());
 					stmt.setInt(2, orderID);
@@ -315,6 +352,12 @@ public class OrderController {
 		return flag;
 	}
 
+	/**
+	 * checks if the order is ongoing
+	 *
+	 * @param carID of order
+	 * @return park station id of current car
+	 */
 	public static int orderOngoingExist(String carID) {
 		int id = -1;
 		PreparedStatement stmt;
@@ -335,7 +378,13 @@ public class OrderController {
 		}
 		return id;
 	}
-	
+
+	/**
+	 * checks if the order is pending
+	 *
+	 * @param carID
+	 * @return true if exists
+	 */
 	public static boolean orderPendingExist(String carID) {
 		boolean flag = false;
 		PreparedStatement stmt;
@@ -356,11 +405,21 @@ public class OrderController {
 		return flag;
 	}
 
+	/**
+	 * @param startTime
+	 * @param endTime
+	 * @return difference between two timestamps in hours
+	 */
 	private static double getTimeDiffInHours(Timestamp startTime, Timestamp endTime) {
 		long diff = endTime.getTime() - startTime.getTime();
 		return diff / 3600000.0;
 	}
 
+	/**
+	 * @param parking_id
+	 * @param type
+	 * @return price per hour of given parking station and order type (subscription, occasional, one-time)
+	 */
 	public static double getPricePerHour(int parking_id, String type) {
 		double price_per_hour = Double.MAX_VALUE;
 		PreparedStatement stmt;
@@ -381,7 +440,12 @@ public class OrderController {
 		return price_per_hour;
 	}
 
-
+	/**
+	 * calculates payment balance when a client wishes to finish parking
+	 *
+	 * @param carID
+	 * @return payment balance (may be negative - means client gets refund)
+	 */
 	public static double calcPriceOnEndOrder(String carID) {
 		double res = Double.MAX_VALUE, price_per_hour = 0, hours = 0;
 
@@ -416,6 +480,11 @@ public class OrderController {
 		return res;
 	}
 
+	/**
+	 * evaluates and updates price of given order (depending on current time)
+	 *
+	 * @param order_id
+	 */
 	public static void calcAndUpdatePrice(int order_id) {
 		double res = Double.MAX_VALUE, price_per_hour = 0, hours = 0;
 		PreparedStatement stmt;
@@ -451,7 +520,12 @@ public class OrderController {
 		}
 	}
 
-
+	/**
+	 * removes order of current order
+	 *
+	 * @param order_id
+	 * @return true if remove succeeded. false otherwise
+	 */
 	public static boolean removeOrderById(int order_id) {
 		java.sql.PreparedStatement stmt;
 		boolean return_res = false;
@@ -473,6 +547,15 @@ public class OrderController {
 	}
 
 	// type ONGOING for end parking, PENDING for cancel
+
+	/**
+	 * removes order of current order
+	 * type ONGOING for end parking, PENDING for cancel
+	 *
+	 * @param carID
+	 * @param type
+	 * @return true if remove succeeded. false otherwise
+	 */
 	public static boolean removeOrder(String carID, String type) {
 		boolean res = false;
 		java.sql.PreparedStatement stmt;
@@ -503,6 +586,12 @@ public class OrderController {
 		return res;
 	}
 
+	/**
+	 * cancels current order
+	 *
+	 * @param orderId
+	 * @return payment balance (depends when the client makes the cancellation)
+	 */
 	public static double cancelOrder(int orderId) {
 		PreparedStatement stmt;
 		double diff = 0, price = Double.MAX_VALUE;
@@ -532,24 +621,32 @@ public class OrderController {
 		}
 		return price;
 	}
-	public static void responseToMessage(int response,int messages_id){
+
+	/**
+	 * updates response to current message
+	 *
+	 * @param response
+	 * @param messages_id
+	 */
+	public static void responseToMessage(int response, int messages_id) {
 		java.sql.PreparedStatement stmt = null;
 		try {
-			stmt =  sql.conn.prepareStatement("UPDATE Messages SET messages_confirmation = ? WHERE messages_id = ?");
+			stmt = sql.conn.prepareStatement("UPDATE Messages SET messages_confirmation = ? WHERE messages_id = ?");
 			stmt.setInt(1, response);
-			stmt.setInt(2, messages_id);    
-	  
+			stmt.setInt(2, messages_id);
+
 			int rs = stmt.executeUpdate();
 
 		} catch (SQLException e1) {
 			e1.printStackTrace();
-		}    
+		}
 
 		if (stmt != null) {
 			try {
 				stmt.close();
 			} catch (SQLException e) {
-			/* ignored */}
+				/* ignored */
+			}
 		}
 
 	}
